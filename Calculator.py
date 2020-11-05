126f52676c75279d7ec7bc4d6981f1767953948a
Spencer Stepanek <spencerstepanek@Spencers-MacBook-Pro-3.local>
2020-11-04 17:47:08 -0700

    This is my commit! I changed Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -60,7 +60,7 @@ while True:
 
         elif choice == "6":
         	num1 = float(input("Enter base number: "))
-        	num2 = float(input("Enter power number: "))
+        	num2 = float(input("Enter power... UNLIMITED POWER number: "))
         	print(str(num1) + " power " + str(num2) + " =" + " "+str(num1 ** num2))
         break
     else: 
